e6b2d8adcc3837f4f22d98e2c3519bb7f8de52ee
wanhwi.son <yeshtml5@gmail.com>
2019-01-28 15:51:27 +0900

    some text modify. and copyright modified

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -5,7 +5,7 @@ import styles from './Copyright.module.scss';
 const Copyright = () => (
     <p className={styles['copyright']}>
         © 2019 wanhwi.son <br/>
-        Powered by <a href="#">GatsbyJS</a> & <a href="#">ReactJS</a> <br/>Hosted by <a href="#">Netlif</a>y Source code on <a href="#" target="_blank">GitHub</a>
+        Powered by <a href="https://www.gatsbyjs.org/" target="_blank">GatsbyJS</a> & <a href="https://reactjs.org/" target="_blank">ReactJS</a> <br/>Hosted by <a href="https://www.netlifycms.org/" target="_blank">Netlif</a>y Source code on <a href="https://github.com/yeshtml5/gatsbyjs" target="_blank">GitHub</a>
     </p>
 );
 
