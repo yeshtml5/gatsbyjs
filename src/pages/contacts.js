2a37ff7b5a0564f1bd6c75924cf350691bff2fac
wanhwi.son <yeshtml5@gmail.com>
2019-02-13 19:45:26 +0900

    add contact us

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -4,23 +4,33 @@ import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 import Page from '../components/Page';
 
-import {Comments, FacebookProvider} from 'react-facebook';
-
 //* lib
 class ContactTemplate extends React.Component {
-    //* function
-    render() {
-        return (
-            <Layout>
-                <Sidebar/>
-                <Page title="Contac me">
-                    <FacebookProvider appId="356027551919150">
-                        <Comments href="http://www.yeshtml5.com"/>
-                    </FacebookProvider>
-                </Page>
-            </Layout>
-        )
-    }
+  componentDidMount() {
+    (function (d, s, id) {
+      let js; const
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=356027551919150&autoLogAppEvents=1&';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  //* function
+  render() {
+    return (
+        <Layout>
+            <div id="fb-root"></div>
+            <Sidebar/>
+            <Page title="Contac me">
+                <div className="fb-comments" data-href="https://yeshtml5.com" data-order-by="reverse_time"
+                     data-numposts="20"/>
+            </Page>
+        </Layout>
+    );
+  }
 }
 
 
